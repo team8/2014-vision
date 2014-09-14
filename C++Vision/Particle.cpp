1b816d9227ff9d909812cb05d6e8115466295070
bencw99 <ben.cohenwang@gmail.com>
2014-09-14 12:14:17 -0700

    Added addPoint and getScore

--- C++Vision/Particle.cpp
+++ C++Vision/Particle.cpp
@@ -1,4 +1,5 @@
 #include "Particle.h"
+#include <math.h>
 
 Particle::Particle(POINT point) {
     this.x = point.x;
@@ -8,4 +9,26 @@ Particle::Particle(POINT point) {
     this.height = 1;
     
     points = new Vector<POINT>();
+    points.push_back(point);
+}
+
+Particle::addPoint(POINT point) {
+    	points.push_back(point);
+
+		if (point.x > x + width) width += point.x - x - width;
+		else if (point.x < x) {
+			double offset = x - point.x;
+			x -= offset;
+			width += offset;
+		}
+		if (point.y > y + height) height += point.y - y - height;
+		else if (point.y < y) {
+			double offset = y - point.y;
+			y -= offset;
+			height += offset;
+		}
+}
+
+Particle::getScore() {
+    		return (int) round((double) points.size() / (width * height) * 100);
 }
