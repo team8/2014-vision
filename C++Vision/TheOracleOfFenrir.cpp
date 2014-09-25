eab3bf8494b26b1fedad52d0db7f706f8a9e0a2d
Tyler Packard <tyler.packard86@gmail.com>
2014-09-25 16:00:19 -0700

    Almost Finished

--- C++Vision/TheOracleOfFenrir.cpp
+++ C++Vision/TheOracleOfFenrir.cpp
@@ -3,15 +3,18 @@
 #include "Vision/BinaryImage.h"
 #include "Math.h"
 
-#define PI = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196442881097566593344612847564823378678316527120190914564856692346034861045432664821339360726024914127372458700660631558817488152092096282925409171536436789259036001133053054882046652138414695194151160943305727036575959195309218611738193261179310511854807446237996274956735188575272489122793818301194912983367336244065664308602139494639522473719070217986094370277053921717629317675238467481846766940513200056812714526356082778577134275778960917363717872146844090122495343014654958537105079227968925892354201995611212902196086403441815981362977477130996051870721134999999837297804995105973173281609631859502445945534690830264252230825334468503526193118817101000313783875288658753320838142061717766914730359825349042875546873115956286388235378759375195778185778053217122680661300192787661119590921642019893809525720106548586327886593615338182796823030195203530185296899577362259941389124972177528347913151557485724245415069595082953311686172785588907509838175463746493931925506040092770167113900984882401285836160356370766010471018194295559619894676783744944825537977472684710404753464620804668425906949129331367702898915210475216205696602405803815019351125338243003558764024749647326391419927260426992279678235478163600934172164121992458631503028618297455570674983850549458858692699569092721079750930295532116534498720275596023648066549911988183479775356636980742654252786255181841757467289097777279380008164706001614524919217321721477235014144197356854816136115735255213347574184946843852332390739414333454776241686251898356948556209921922218427255025425688767179049460165346680498862723279178608578438382796797668145410095388378636095068006422512520511739298489608412848862694560424196528502221066118630674427862203919494504712371378696095636437191728746776465757396241389086583264599581339047802759009946576407895126946839835259570982582262052248940772671947826848260147699090264013639443745530506820349625245174939965143142980919065925093722169646151570985838741059788595977297549893016175
+#define PI = 3.14159265
 
 #define MAX_PARTICLES 10
 
-#define HALF_CAMERA_ANGLE (56 / 360.0 * PI) // Axis M1013 is about 67 degrees, but this might be correct
+#define HALF_CAMERA_ANGLE 56 // Axis M1013 is about 67 degrees, but this might be correct
 #define ERROR_MARGIN 3
 #define RECTANGULARITY_LIMIT 40
 #define ASPECT_RATIO_LIMIT 55
 #define MINIMUM_AREA 150
+#define TAPE_WIDTH_LIMIT 50
+#define VERTICAL_SCORE_LIMIT 50
+#define LR_SCORE_LIMIT 50
 
 class RobotDemo : public SimpleRobot {
 	RobotDrive myRobot;
@@ -26,7 +29,7 @@ class RobotDemo : public SimpleRobot {
 	struct Target {
 			int verticalIndex;
 			int horizontalIndex;
-			bool Hot;
+			bool hot;
 			double totalScore;
 			double leftScore;
 			double rightScore;
@@ -47,7 +50,7 @@ public:
 		Target target;
 		int verticalTargets[MAX_PARTICLES];
 		int horizontalTargets[MAX_PARTICLES];
-		int verticalTargetCount, horizontalTargetCount;
+		int numVertTargets, numHorizTargets;
 		AxisCamera &camera = AxisCamera::GetInstance();
 		ParticleFilterCriteria2 maskCriteria[] = {{IMAQ_MT_AREA, MINIMUM_AREA, 65535, false, false}};
 		
@@ -57,7 +60,7 @@ public:
 		BinaryImage *maskedImage = mask->ParticleFilter(maskCriteria, 1);
 		
 		vector<ParticleAnalysisReport> *reports = maskedImage->GetOrderedParticleAnalysisReports();
-		verticalTargetCount = horizontalTargetCount = 0;
+		numVertTargets = numHorizTargets = 0;
 		
 		
 		if (reports -> size() > 0) {
@@ -66,11 +69,99 @@ public:
 			for (int i = 0; i < MAX_PARTICLES && i < reports->size(); i++) {
 				ParticleAnalysisReport *report = &(reports -> at(i));
 				
-				scores[i].rectangularity = scoreRectangularity(report);
-				scores[i].aspectRatioVertical = scoreAspectRatio(filteredImage, report, true);
-				scores[i].aspectRatioHorizontal = scoreAspectRatio(filteredImage, report, false);
+				scores[i].rectangularity = getRectScore(report);
+				scores[i].aspectRatioVertical = getAspectRatioScore(maskedImage, report, true);
+				scores[i].aspectRatioHorizontal = getAspectRatioScore(maskedImage, report, false);
+				
+				if (scores[i].rectangularity > RECTANGULARITY_LIMIT && scores[i].aspectRatioHorizontal > ASPECT_RATIO_LIMIT) {
+					horizontalTargets[numHorizTargets++] = i;
+				} else if (scores[i].rectangularity > RECTANGULARITY_LIMIT && scores[i].aspectRatioVertical > ASPECT_RATIO_LIMIT) {
+					verticalTargets[numVertTargets++] = i;
+				}
 			}
+			
+			target.totalScore = target.leftScore = target.rightScore = target.tapeWidthScore = target.verticalScore = 0;
+			target.verticalIndex = verticalTargets[0];
+			for (int v = 0; v < numVertTargets; v++) {
+				ParticleAnalysisReport *verticalReport = &(reports -> at(verticalTargets[v]));
+				for (int h = 0; h < numHorizTargets; h++) {
+					ParticleAnalysisReport *horizontalReport = &(reports -> at(horizontalTargets[h]));
+					double horizWidth, vertWidth, horizHeight;
+					imaqMeasureParticle(maskedImage -> GetImaqImage(), horizontalReport -> particleIndex, 0, IMAQ_MT_EQUIVALENT_RECT_LONG_SIDE, &horizWidth);
+					imaqMeasureParticle(maskedImage -> GetImaqImage(), verticalReport -> particleIndex, 0, IMAQ_MT_EQUIVALENT_RECT_SHORT_SIDE, &vertWidth);
+					imaqMeasureParticle(maskedImage -> GetImaqImage(), horizontalReport -> particleIndex, 0, IMAQ_MT_EQUIVALENT_RECT_SHORT_SIDE, &horizHeight);
+					
+					double leftScore = valueToScore(1.2*(verticalReport->boundingRect.left - horizontalReport->center_mass_x)/horizWidth);
+					double rightScore = valueToScore(1.2*(horizontalReport->center_mass_x - verticalReport->boundingRect.left - verticalReport->boundingRect.width)/horizWidth);
+					double tapeWidthScore = valueToScore(vertWidth/horizHeight);
+					double verticalScore = valueToScore(1-(verticalReport->boundingRect.top - horizontalReport->center_mass_y)/(4*horizHeight));
+					
+					double total = leftScore > rightScore ? leftScore : rightScore;
+					total += tapeWidthScore + verticalScore;
+					
+					if (total > target.totalScore) {
+						target.horizontalIndex = horizontalTargets[h];
+						target.verticalIndex = verticalTargets[v];
+						target.totalScore = total;
+						target.leftScore = leftScore;
+						target.rightScore = rightScore;
+						target.tapeWidthScore = tapeWidthScore;
+						target.verticalScore = verticalScore;
+					}
+				}
+				target.hot = isHot(target);
+			}
+			
+			delete maskedImage;
+			delete mask;
+			delete image;
+			delete scores;
+			delete reports;
 		}
+	}
+	
+	double getRectScore(ParticleAnalysisReport *report){
+		if(report -> boundingRect.width * report -> boundingRect.height != 0){
+			return 100 * report -> particleArea / (report -> boundingRect.width * report -> boundingRect.height);
+		}
+		return 0;
+	}
+	
+	double getAspectRatioScore(BinaryImage *image, ParticleAnalysisReport *report, bool vertical){
+		double rectLong, rectShort, idealAspectRatio, aspectRatio;
+		idealAspectRatio = vertical ? (4.0/32) : (23.5/4);
+		
+		imaqMeasureParticle(image -> GetImaqImage(), report -> particleIndex, 0, IMAQ_MT_EQUIVALENT_RECT_LONG_SIDE, &rectLong);
+		imaqMeasureParticle(image -> GetImaqImage(), report -> particleIndex, 0, IMAQ_MT_EQUIVALENT_RECT_SHORT_SIDE, &rectShort);
+		
+		if(report -> boundingRect.width > report -> boundingRect.height){
+			aspectRatio = valueToScore(((rectLong / rectShort) / idealAspectRatio));
+		} else {
+			aspectRatio = valueToScore(((rectShort / rectLong) / idealAspectRatio));
+		}
+		return aspectRatio;
+	}
+	
+	double valueToScore(double value) {
+		return round(max(0, min(100, (1 - fabs(1 - value)) * 100)));
+	}
+	
+	bool isHot(Target target) {
+		bool hot = target.tapeWidthScore >= TAPE_WIDTH_LIMIT;
+		hot &= target.verticalScore >= VERTICAL_SCORE_LIMIT;
+		hot &= (target.leftScore > LR_SCORE_LIMIT) || (target.rightScore > LR_SCORE_LIMIT);
+		
+		return hot;
+	}
+	
+	double getDistance (BinaryImage *image, ParticleAnalysisReport *report) {
+		double rectLong, height;
+		
+		imaqMeasureParticle(image->GetImaqImage(), report->particleIndex, 0, IMAQ_MT_EQUIVALENT_RECT_LONG_SIDE, &rectLong);
+		height = min(report -> boundingRect.height, rectLong);
+		
+		double cameraRadians = HALF_CAMERA_ANGLE * PI / 360;
+		return 15360 / (height * 24 * tan(cameraRadians));
 	}
 
 	void OperatorControl() {
@@ -83,4 +174,3 @@ public:
 };
 
 START_ROBOT_CLASS(RobotDemo);
-
