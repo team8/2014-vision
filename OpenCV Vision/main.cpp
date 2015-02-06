aad540fd2cdd700f70e295e35fdaa1fb1302e4e4
TPackard <tyler.packard86@gmail.com>
2015-02-05 17:26:38 -0800

    Moved big comment to readme and added a few more comments

--- OpenCV Vision/main.cpp
+++ OpenCV Vision/main.cpp
@@ -10,36 +10,7 @@
  * for Recycle Rush 2015
  * Written by Tyler Packard with help from Ben Cohen-Wang
  *
- * OVERVIEW:
- * This program identifies yellow totes during autonomous mode and calculates the distance and angle to the tote. It
- * works by shining a bright green light at the retro-reflective tape on the yellow totes and capturing the reflected
- * in a camera mounted on the robot. The camera takes low exposure video and darkens the images before sending them to
- * this program. Once this program receives the images, it masks out the retro-reflective based on predetermined hue,
- * saturation, and value (HSV) ranges. Then all of the masked blobs are converted to polygons and non-reflective strip
- * shaped polygons are removed.
- *
- * CALCULATIONS:
- * The distance to yellow totes is calculated based off of the ratio of the width of the reflective strips to the width
- * of the camera's field of view. The angle to the tote is calculated by finding the arccosine of the actual width of
- * the strips divided by the expected width of the strips if the angle was 0 degrees. For more in-depth explainations,
- * see the comments above the actual methods.
- *
- * USAGE:
- * The values found by this program will be went by the raspberry pi to the RoboRio. They will be used by the autonomous
- * controller to direct the robot to the yellow totes. This allows us to create tote sets and stacked tote sets. Tote
- * sets exist if all three yellow totes are in the auto zone, but are not stacked, and it awards the alliance 6 points.
- * Stacked tote sets exist if all three yellow totes are in the auto zone and stacked in a legal formation, awarding the
- * alliance 20 points.
- *
- * WARNINGS:
- * The angle finding formula does not return accurate angles when the robot is close to the totes (roughly less than 7.5
- * feet) and at a shallow angle to the tote (roughly less than 15 degrees). This happens because the the cosines of
- * smaller angles are closer together than the cosines of larger angles. This means that the arccosine function requires
- * increasingly accurate measurments the closer the angle is to 0 degrees, and our camera cannot provide such accurate
- * measurements.
- * Another problem with this code is that it becomes very difficult to detect the retro-reflective strips from over 15
- * feet away. This happens because the resolution of our camera is too low, and the reflective strips are too small in
- * the image to detect reliably.
+ * See README.md for more details
  */
 
 #define CAMERA_ANGLE 70 / 360.0 * M_PI // The angle of the camera's FOV in radians
@@ -50,8 +21,8 @@ using namespace std;
 using Contour = vector<Point>; // Contours are collections of points
 
 // Global variables
-queue<double> angles;
-double totalAng = 0;
+queue<double> angles; // The last 10 angles calculated (For calculating the average)
+double totalAng = 0; // The sum of the angles from the queue above
 
 Mat loadImage(string filePath) {
     Mat image;
@@ -122,8 +93,7 @@ double getDist(Mat image, vector<Contour> approxPolys) {
         int maxVertDiff = 0;
         for (int i = 0; i < poly.size() - 1; i++) {
             int vertDiff = abs(poly[i].y - poly[i+1].y);
-            if (vertDiff > maxVertDiff)
-                maxVertDiff = vertDiff;
+            if (vertDiff > maxVertDiff) maxVertDiff = vertDiff;
         }
         int vertDiff = abs(poly[0].y - poly[poly.size()-1].y); // Repeat for the first and last points
         if (vertDiff > maxVertDiff)
